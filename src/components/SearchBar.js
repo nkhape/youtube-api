3184c755eb1fc976beafee7df8f9cee2f1b9638d
Nicholas Hape <hape.nick@gmail.com>
2021-02-22 16:29:57 +0100

    added SearchBar and App components

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,33 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+state = { term: '' };
+
+onInputChange = (event) => {
+  this.setState({ term: event.target.value });
+};
+
+onFormSubmit = (event) => {
+  event.preventDefault();
+
+  // TODO: make sure we call callback from parent component
+};
+
   render () {
     return (
-      <div> SearchBar </div>
+      <div className="search-bar ui segment">
+        <form onSumbit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Find a Vid' 📽</label>
+            <input
+              type="text"
+              placeholder="hallöchen"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
     );
   }
 }
