dc7db1fc5c305ac7beb37ad3d069671b8aac7f90
Nick Hape <hape.nick@gmail.com>
2021-02-24 16:15:31 +0100

    added iframe player, dynamic update of video, title, description when pressed

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import VideoList from './VideoList';
 import youtube from '../apis/youtube';
+import VideoDetail from './VideoDetail';
+
 
 const KEY = "AIzaSyAPt-bjJ2ncaeK_Q45vLmoOKJ2WwyuOaF8";
 
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
   onTermSubmit = async term => {
     const response = await youtube.get("/search", {
       params: {
@@ -17,14 +19,27 @@ class App extends React.Component {
         key: KEY
       }
     });
-
     this.setState({ videos: response.data.items});
   };
+
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
+  };
+
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
